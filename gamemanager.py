c26a64d773a58397dfac356a782baa7a4b767c8e
Badsoul01 <d.jurena@gmail.com>
2026-05-14 11:04:14 +0200

    založení init pro GameManagera a vypsaní importu

--- gamemanager.py
+++ gamemanager.py
@@ -1,2 +1,11 @@
+import curses
+from gamemenu import GameMenu
+from game import Game
+from config import CURSES_COLORS
+
 class GameManager:
-    pass
+
+
+    def __init__(self):
+        self.menu = GameMenu()
+        self.state = "MENU"
