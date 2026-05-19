862978c35f15ccaaeb52fb7338579a6eef8c50e8
Badsoul01 <d.jurena@gmail.com>
2026-05-19 14:57:29 +0200

    předělání třídy na možnosti load_game

--- block.py
+++ block.py
@@ -4,14 +4,19 @@ from config import SHAPES,COLOR_MAP
 
 class Tetromino:
 
-    def __init__(self, game):
+    def __init__(self, game,init_name = None,init_shape = None,init_x = None,init_y=None):
         self.shapes = SHAPES
         self.keys = list(self.shapes.keys())
-        self.name = random.choice(self.keys)
+        self.name = init_name if init_name else random.choice(self.keys)
         self.color_id = COLOR_MAP[self.name]
-        self.relative_blocks = self.shapes[self.name]
-        self.x= (game.board.width//2)-1
-        self.y= 1
+        self.relative_blocks = init_shape if init_shape else  self.shapes[self.name]
+
+        if init_x is not None and init_y is not None:
+            self.x = init_x
+            self.y = init_y
+        else:
+            self.x = (game.board.width // 2) - 1
+            self.y= 1
 
 
     def get_world_coordinates(self):
