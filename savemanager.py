00babcb746d5ce0a3adf074766b8d34ec78e09f0
Badsoul01 <d.jurena@gmail.com>
2026-05-19 17:02:16 +0200

    přidána metoda pro nahráni hry

--- savemanager.py
+++ savemanager.py
@@ -5,7 +5,7 @@ class SaveManager:
         self.filename = filename
 
 
-    def savegame(self,game):
+    def save_game(self,game):
         grid_date = {}
 
         for (x,y),symbol in game.board.grid.items():
@@ -15,7 +15,8 @@ class SaveManager:
             "score_manager": {
                 "score" : game.score_manager.score,
                 "deleted_lines": game.score_manager.deleted_lines,
-                "level": game.score_manager.level
+                "level": game.score_manager.level,
+                "starting_level":game.score_manager.starting_level
             },
             "falling_block":{
                 "name": game.falling_block.name,
@@ -36,4 +37,21 @@ class SaveManager:
         with open(self.filename, "w", encoding="utf-8") as f:
             json.dump(save, f, indent=4)
 
-            
+
+    def load_game(self):
+
+        with open(self.filename, "r", encoding="utf-8") as file:
+             saved_data = json.load(file)
+
+
+        original_grid = {}
+        for key,symbol in saved_data["grid"].items():
+            x_str, y_str = key.split(",")
+            original_grid[(int(x_str),int(y_str))]=symbol
+
+        saved_data["grid"] = original_grid
+        return  saved_data
+
+
+
+
