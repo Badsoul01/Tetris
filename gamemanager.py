cb0ebc9a38bd1d72dd0fb774ba0483290ac21fa3
Badsoul01 <d.jurena@gmail.com>
2026-05-28 15:45:21 +0200

    přidání metody pro ukladaní dat do db

--- gamemanager.py
+++ gamemanager.py
@@ -20,6 +20,14 @@ class GameManager:
         pygame.mixer.music.load("tetris_theme.mp3")
         pygame.mixer.music.play(-1)
 
+    def _save_data(self):
+        name = self.tetris.player_name
+        score = self.tetris.score_manager.score
+        level = self.tetris.score_manager.level
+        pieces = self.tetris.score_manager.count_pieces
+        rows = self.tetris.score_manager.deleted_lines
+        self.db.save_score(name, score, level, pieces,rows)
+
     def run_game(self,stdscr):
         curses.curs_set(0)
         curses.start_color()
@@ -49,9 +57,13 @@ class GameManager:
                     self.tetris = Game(self.menu.settings["starting_level"], load_data=load_files)
                     self.tetris.color_scheme = self.menu.settings["colors"]
                     self.tetris.ghost_brick = self.menu.settings["ghost_brick"]
+                    self.tetris.highest_score = self.db.get_highest_score()
                     action = self.tetris.game_loop(stdscr)
 
-                    if action == "GAME STOP" or action == "MENU":
+                    if action == "GAME STOP":
+                        self._save_data()
+                        self.state = "MENU"
+                    elif action == "MENU":
                         self.state = "MENU"
 
                 case "NEW GAME":
@@ -60,11 +72,16 @@ class GameManager:
                     self.tetris = Game(self.menu.settings["starting_level"])
                     self.tetris.color_scheme = self.menu.settings["colors"]
                     self.tetris.ghost_brick = self.menu.settings["ghost_brick"]
+                    self.tetris.highest_score = self.db.get_highest_score()
+                    self.tetris.top_ten = self.db.top_ten()
 
                     action = self.tetris.game_loop(stdscr)
-                    if action == "GAME STOP" or action == "MENU":
+                    if action == "GAME STOP":
+                        self._save_data()
                         self.state = "MENU"
 
+                    elif action == "MENU":
+                        self.state = "MENU"
 
                 case "EXIT GAME":
                     break
